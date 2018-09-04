28e58a020f80e3bf2126fbbe4f63c48986f6a6ca
Kevin Henke <henkekm@gmail.com>
2018-09-03 22:59:35 -0400

    Remove filling/emptying user

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <Navbar v-on:log-user-out="emptyUser" />
-    <router-view :favoriteTeam=this.favoriteTeam :teams=this.teams :user=this.user v-on:log-user-in="fillUser" />
+    <Navbar />
   </div>
 </template>
 
@@ -30,35 +29,9 @@ export default {
       user: null
     }
   },
-  methods: {
-    emptyUser () {
-      console.log('emptying user')
-      this.user = null
-    },
-    fillUser (_uid) {
-      console.log('filling user', _uid)
-      let userRef = db.collection('users').where('uid', '==', _uid).limit(1)
-      userRef.get().then(snapshot => {
-        snapshot.forEach(doc => {
-          let profile = doc.data()
-          profile.id = doc.id
-          this.user = profile
-        })
-      }).then(() => {
-        let favoriteTeamRef = db.collection('teams').doc(this.user.favoriteTeamId)
-        favoriteTeamRef.get().then(doc => {
-          let favoriteTeam = doc.data()
-          favoriteTeam.id = doc.id
-          this.favoriteTeam = favoriteTeam
-        })
-        console.log('user successfully filled')
-      })
-    }
-  },
-  created () {
-    let teamsRef = db.collection('teams')
-    let teams = []
-    teamsRef.get().then(snapshot => {
+  beforeCreate () {
+    let userRef = db.collection('users').where('uid', '==', firebase.auth().currentUser.uid).limit(1)
+    userRef.get().then(snapshot => {
       snapshot.forEach(doc => {
         let team = doc.data()
         team.id = doc.id
@@ -66,22 +39,6 @@ export default {
       })
       this.teams = teams
     })
-  },
-  beforeUpdate () {
-    console.log('beforeUpdate is firing')
-    if (this.user) {
-    } else {
-      // If the app is created when the user is not logged in
-      if (firebase.auth().currentUser) {
-        // This will spit an error
-        // Somehow, fillUser is firing before currentUser.uid exists?
-        let uid = firebase.auth().currentUser.uid
-        console.log('firebase.auth().currentUser.uid', firebase.auth().currentUser.uid)
-        if (uid) {
-          this.fillUser(uid)
-        }
-      }
-    }
   }
 }
 </script>
--- src/components/auth/Signin.vue
+++ src/components/auth/Signin.vue
@@ -36,8 +36,6 @@ export default {
         this.feedback = null
         firebase.auth().signInWithEmailAndPassword(this.email, this.password)
           .then(() => {
-            console.log('signin is firing')
-            this.$emit('log-user-in')
             this.$router.push({ name: 'Week', params: { week_number: '1' } })
           })
           .catch(err => {
