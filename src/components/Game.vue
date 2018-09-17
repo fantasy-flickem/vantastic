aa9f4cb8892459548ba9b4027119e16ce4670d97
Kevin Henke <henkekm@gmail.com>
2018-09-17 01:39:22 -0400

    Surface data in new Picksplit component
    TODO create callout functionality and style component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,16 +2,7 @@
   <div class="game">
     <div class="button__group button__group--horizontal button__group--justify-content-space-between" style="width:100%;">
       <Team :_team='_game.awayTeam' :_game='_game' :_pick='pick' :_score='_game.awayTeamScore' @send-selection='makeOrUpdatePick($event)'></Team>
-      <!-- <button style="height:65px; background-color:#FCFEFF">
-        <svg width="25" height="65" version="1" xmlns="http://www.w3.org/2000/svg">
-          <rect x="0" y="0" width="25" height="12.5" transform="rotate(180, 12.5, 32.5) translate(0, 15)" fill="#B1012F"></rect>
-          <text x="12.5" y="65" text-anchor="middle" class="text text--handegg-text text--fs-extra-small">25%</text>
-        </svg>
-        <svg width="25" height="65" version="1" xmlns="http://www.w3.org/2000/svg">
-          <rect x="0" y="0" width="25" height="37.5" transform="rotate(180, 12.5, 32.5) translate(0, 15)" fill="#428FC1"></rect>
-          <text x="12.5" y="65" text-anchor="middle" class="text text--handegg-text text--fs-extra-small">75%</text>
-        </svg>
-      </button> -->
+      <Picksplit :_tribePicks='tribePicks' :_userNameDictionary='_userNameDictionary'></Picksplit>
       <Team :_team='_game.homeTeam' :_game='_game' :_pick='pick' :_score='_game.homeTeamScore' @send-selection='makeOrUpdatePick($event)'></Team>
     </div>
   </div>
@@ -20,20 +11,23 @@
 <script>
 import db from '@/firebase/init'
 import firebase from 'firebase'
+import Picksplit from './Picksplit'
 import Team from './Team'
 export default {
   name: 'Game',
-  components: { Team },
+  components: { Picksplit, Team },
   props: [
     '_currentlyViewedWeekNumber',
     '_game',
     '_isFavoriteTeamGame',
     '_pick',
-    '_user'
+    '_user',
+    '_userNameDictionary'
   ],
   data () {
     return {
-      pick: this._pick
+      pick: this._pick,
+      tribePicks: { away: [], home: [] }
     }
   },
   methods: {
@@ -76,6 +70,20 @@ export default {
         }
       })
     }
+  },
+  created () {
+    let picksRef = db.collection('picks').where('tribeId', '==', this._user.tribeId).where('gameId', '==', this._game.id)
+    picksRef.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        let pick = doc.data()
+        pick.id = doc.id
+        if (pick.teamId === this._game.awayTeamId) {
+          this.tribePicks.away.push(pick)
+        } else {
+          this.tribePicks.home.push(pick)
+        }
+      })
+    })
   }
 }
 </script>
---
+++ src/components/Picksplit.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="picksplit" style="position:relative; height:65px;">
+    <button style="height:65px; background-color:#FCFEFF" @click='displayPicksplitByName()'>
+      <svg width="25" height="65" version="1" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="25" :height="awayBarHeight" transform="rotate(180, 12.5, 32.5) translate(0, 15)" fill="#B1012F"></rect>
+        <text x="12.5" y="65" text-anchor="middle" class="text text--handegg-text text--fs-extra-small">{{ _tribePicks.away.length }}</text>
+      </svg>
+      <svg width="25" height="65" version="1" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="25" :height="homeBarHeight" transform="rotate(180, 12.5, 32.5) translate(0, 15)" fill="#428FC1"></rect>
+        <text x="12.5" y="65" text-anchor="middle" class="text text--handegg-text text--fs-extra-small">{{ _tribePicks.home.length }}</text>
+      </svg>
+    </button>
+    <!-- <div class="callout" style="position:absolute; top:0; right:50%;">
+      <div class="text">{{ _tribePicks.away.map(awayPicks => this._userNameDictionary[awayPicks.uid]) }}</div>
+      <div class="text">{{ _tribePicks.home.map(homePicks => this._userNameDictionary[homePicks.uid]) }}</div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Picksplit',
+  props: [ '_tribePicks', '_userNameDictionary' ],
+  data () {
+    return { }
+  },
+  computed: {
+    totalPicks () {
+      return this._tribePicks.away.length + this._tribePicks.home.length
+    },
+    awayBarHeight () {
+      return (this._tribePicks.away.length / this.totalPicks) * 50
+    },
+    homeBarHeight () {
+      return (this._tribePicks.home.length / this.totalPicks) * 50
+    }
+  },
+  created () {
+    console.log('Picksplit.created is firing')
+  },
+  methods: {
+    displayPicksplitByName () {
+      console.log('displayPicksplitByName', this._tribePicks)
+    }
+  }
+}
+</script>
