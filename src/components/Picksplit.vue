bc2ac8bce9734827015ce832b0507c7dfcbdd2f4
Kevin Henke <henkekm@gmail.com>
2018-09-19 22:57:51 -0400

    Toggle pick name list callout when clicking picksplit

--- src/components/Picksplit.vue
+++ src/components/Picksplit.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="picksplit" style="position:relative; height:65px;">
-    <button style="height:65px; background-color:#FCFEFF" @click='displayPicksplitByName()'>
+  <div class="picksplit" style="height:65px;">
+    <button style="height:65px; background-color:#FCFEFF" @click.stop.prevent='displayPicksplitByName'>
       <svg width="25" height="65" version="1" xmlns="http://www.w3.org/2000/svg">
         <rect x="0" y="0" width="25" :height="awayBarHeight" transform="rotate(180, 12.5, 32.5) translate(0, 15)" fill="#B1012F"></rect>
         <text x="12.5" y="65" text-anchor="middle" class="text text--handegg-text text--fs-extra-small">{{ _tribePicks.away.length }}</text>
@@ -10,19 +10,35 @@
         <text x="12.5" y="65" text-anchor="middle" class="text text--handegg-text text--fs-extra-small">{{ _tribePicks.home.length }}</text>
       </svg>
     </button>
-    <!-- <div class="callout" style="position:absolute; top:0; right:50%;">
-      <div class="text">{{ _tribePicks.away.map(awayPicks => this._userNameDictionary[awayPicks.uid]) }}</div>
-      <div class="text">{{ _tribePicks.home.map(homePicks => this._userNameDictionary[homePicks.uid]) }}</div>
-    </div> -->
+    <div :id=calloutId class="callout">
+      <div class="callout__content">
+        <div class="callout__away-pickers">
+          <div v-for='(awayPicker, index) in tribePickNames.away' :key=index class="text">{{ awayPicker }}</div>
+        </div>
+        <div class="callout__home-pickers">
+          <div v-for='(homePicker, index) in tribePickNames.home' :key=index class="text">{{ homePicker }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
 export default {
   name: 'Picksplit',
-  props: [ '_tribePicks', '_userNameDictionary' ],
+  props: [
+    '_gameId',
+    '_tribePicks',
+    '_user',
+    '_userNameDictionary'
+  ],
   data () {
-    return { }
+    return {
+      calloutId: 'callout-id-' + this._gameId,
+      tribeDisplayNameDictionary: {},
+      tribePickNames: {away: [], home: []}
+    }
   },
   computed: {
     totalPicks () {
@@ -36,11 +52,39 @@ export default {
     }
   },
   created () {
-    console.log('Picksplit.created is firing')
+    let tribeUsersRef = db.collection('users').where('tribeId', '==', this._user.tribeId).orderBy('score', 'desc')
+    let tribeDisplayNameDictionary = {}
+    tribeUsersRef.get().then(snapshot => {
+      let tribeUsersArray = []
+      snapshot.forEach(doc => {
+        let tribeUser = doc.data()
+        tribeUser.id = doc.id
+        tribeUsersArray.push(tribeUser)
+        tribeDisplayNameDictionary[tribeUser.uid] = tribeUser.displayName
+      })
+      this.tribeDisplayNameDictionary = tribeDisplayNameDictionary
+    })
   },
   methods: {
-    displayPicksplitByName () {
-      console.log('displayPicksplitByName', this._tribePicks)
+    loadPicksplitNames () {
+      this._tribePicks.away.forEach(awayTribePick => {
+        this.tribePickNames.away.push(this.tribeDisplayNameDictionary[awayTribePick.uid])
+      })
+      this._tribePicks.home.forEach(homeTribePick => {
+        this.tribePickNames.home.push(this.tribeDisplayNameDictionary[homeTribePick.uid])
+      })
+    },
+    displayPicksplitByName (event) {
+      let picksAreLoaded = this.tribePickNames.away.length > 0 || this.tribePickNames.home.length > 0
+      let openCallout = document.querySelector('#' + this.calloutId)
+      if (!picksAreLoaded) {
+        this.loadPicksplitNames()
+        if (this.tribePickNames.away.length > 0 || this.tribePickNames.home.length > 0) {
+          openCallout.classList.toggle('open')
+        }
+      } else {
+        openCallout.classList.toggle('open')
+      }
     }
   }
 }
